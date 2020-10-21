1c6a54ddd369fd0a4fd3c1bf0f451f104864dd50
Caetano Albuquerque <44181454+caetanobca@users.noreply.github.com>
2020-10-21 14:24:59 -0300

    Add files via upload

---
+++ Haskell/CadastroDeImpedimentos.hs
@@ -0,0 +1,12 @@
+data Impedimento = Medicamento{ 
+     funcao :: String
+    ,composto :: String
+    ,tempoSuspencao :: String
+} | Doenca{
+     cid :: String
+    ,tempoSuspencao :: String
+} deriving (Show)
+
+
+cadastraImpedimento :: [Impedimento] -> Impedimento -> [Impedimento]
+cadastraImpedimento lista impedimento = lista ++ [impedimento]
