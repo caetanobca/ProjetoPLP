fe9721fcebe70dba51e16578033eee9c216ee3fe
Caio Davi <caio.silva@ccc.ufcg.edu.br>
2020-10-26 08:52:47 -0300

    alterando main,auxiliar e enfermeiro

--- Haskell/Auxiliar.hs
+++ Haskell/Auxiliar.hs
@@ -1,9 +1,17 @@
 module Auxiliar where
 
 import qualified Impedimento as Impedimento
+import qualified Enfermeiro as Enfermeiro
+import Data.Map as Map
 
 --Esse metodo vai carregar os empedimentos que estavam salvos em um arquivo
 iniciaImpedimentos :: [Impedimento.Impedimento]
 iniciaImpedimentos = [(Impedimento.Doenca "123" "2 meses"), (Impedimento.Medicamento "Funcao1" "Composto1" "25 meses")]
+
+iniciaEnfermeiros :: [Enfermeiro.Enfermeiro]
+iniciaEnfermeiros = [(Enfermeiro.Enfermeiro "Caio Davi" "Rua Marechal" 21 "33224455"), (Enfermeiro.Enfermeiro "Mateus" "Rua General" 23 "33224455")]
+
+iniciaEscala :: Map String String
+iniciaEscala = Map.fromList [("16/10", "José"), ("17/10","Pedro")]
 
 --Implementar metodo q vai salvar a lista de impedimentos
