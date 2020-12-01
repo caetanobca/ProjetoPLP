5f0aa51037b9864eb8d0184bc5021b9932cb30b5
Caio Davi <caio.silva@ccc.ufcg.edu.br>
2020-12-01 18:26:06 -0300

    mudanças da tarde

--- Prolog/Doador.pl
+++ Prolog/Doador.pl
@@ -2,9 +2,10 @@
 
 constroiDoador(Nome,Endereco,Idade,Telefone,TipSanguineo,ImpedimentoStr,UltimoDiaImpedido,doador(Nome,Endereco,Idade,Telefone,TipSanguineo,ImpedimentoStr,UltimoDiaImpedido)).
 
-getOntem(Ontem):- 
+getOntemString(DataString):- 
     get_time(Stamp),
-    Ontem is Stamp - 86400.
+    Ontem is (Stamp - 86400),
+    dataParaString(Ontem,DataString).
 
 
 %salvaDoador(Doador,Lista,Result):- append(Lista,[Doador],Result).
@@ -29,20 +30,50 @@ listarDoadores([]):-nl.
 listarDoadores([Head|Tail]):- doadoresToString(Head,DoadoresString), write(DoadoresString), nl, listarDoadores(Tail).
 
 %toString Doadores
-doadoresToString(doador(Nome,Endereco,Idade,Telefone, TipSanguineo, ImpedimentoStr, UltimoDiaImpedido), Result):- string_concat("Nome: ", Nome, Parte1), string_concat(Parte1, " Endereço: ", Parte2),
-                    string_concat(Parte2, Endereco, Parte3),string_concat(Parte3, " Idade: ", Parte4), string_concat(Parte4, Idade, Parte5), 
-                    string_concat(Parte5, " Telefone: ", Parte6), string_concat(Parte6, Telefone, Parte7), string_concat(Parte7, " Tipo Sanguineo: ", Parte8),
-                    string_concat(Parte8, TipSanguineo, Parte9), string_concat(Parte9, " Impedimentos: ", Parte10), string_concat(Parte10, ImpedimentoStr, Parte11),
-                    string_concat(Parte11, " Ultimo Dia Impedido: ", Parte12), stamp_date_time(UltimoDiaImpedido, dataString, 'UTC'), string_concat(Parte12, dataString, Result).
+doadoresToString(doador(Nome,Endereco,Idade,Telefone, TipSanguineo, ImpedimentoStr, UltimoDiaImpedido), Result):- 
+    string_concat("Nome: ", Nome, Parte1), string_concat(Parte1, " Endereço: ", Parte2),
+    string_concat(Parte2, Endereco, Parte3),string_concat(Parte3, " - Idade: ", Parte4), string_concat(Parte4, Idade, Parte5), 
+    string_concat(Parte5, " - Telefone: ", Parte6), string_concat(Parte6, Telefone, Parte7), string_concat(Parte7, " - Tipo Sanguineo: ", Parte8),
+    string_concat(Parte8, TipSanguineo, Parte9), string_concat(Parte9, " - Impedimentos: ", Parte10), string_concat(Parte10, ImpedimentoStr, Parte11),
+    string_concat(Parte11, " - Ultimo Dia Impedido: ", Parte12),string_concat(Parte12,UltimoDiaImpedido,Result).
 
 existeDoador(Nome,ListaDoador):- buscaDoador(Nome,ListaDoador,Result), \+(Result = "Doador não encontrado").
 removerDoador(ListaDoador,Nome,Result):- buscaDoador(Nome,ListaDoador,Doador), delete(ListaDoador,Doador,Result).
 
 %Metodo que vai Adiciona o ultimo dia impedido
-adicionaImpedimento(Doador, novoImpedimento, result):- getDoadorImpedimentoStr(Doador, ImpedimentoStr), string_concat(ImpedimentoStr, "// ", parte1),
-string_concat(parte1, novoImpedimento, final), getUltimoDiaImpedido(NovoImpedimento, StampAtual, NovoStamp),
-result = constroiDoador(Nome,Endereco,Idade,Telefone,TipSanguineo, TodosImpedimentos, NovoStamp,Doador).
+adicionaImpedimento(Doador, NovoImpedimento, Result):- 
+    getDoadorImpedimentoStr(Doador, ImpedimentoStr), getDoadorNome(Doador, Nome),
+    getDoadorEndereco(Doador, Endereco), getDoadorIdade(Doador, Idade), getDoadorTelefone(Doador, Telefone),
+    getDoadorTipSanguineo(Doador, TipSanguineo), 
+    getDoadorUltimoDiaImpedido(Doador, UltimoDiaImpedido), stringParaData(UltimoDiaImpedido,Stamp),
+    string_concat(ImpedimentoStr, "// ", Parte1), impedimentoToStringSimples(NovoImpedimento,ImpedimentoStrNovo) ,
+    string_concat(Parte1, ImpedimentoStrNovo, Final), 
+    getUltimoDiaImpedido(NovoImpedimento, Stamp, NovoStamp), dataParaString(NovoStamp, NovaData),
+    constroiDoador(Nome,Endereco,Idade,Telefone,TipSanguineo,Final,NovaData,Result).
+
+%Metodo que verifica se o doador está impedido de doar
+estaImpedido(Doador, DiaDoacao) :- 
+    getDoadorUltimoDiaImpedido(Doador,UltimoDiaImpedido), 
+    stringParaData(DiaDoacao, DiaDoacaoStamp),
+    stringParaData(UltimoDiaImpedido, UltimoDiaImpedidoStamp),
+    %nl,write("->"),write(DiaDoacaoStamp),
+    %nl,write("->"),write(UltimoDiaImpedidoStamp),
+    DiaDoacaoStamp < UltimoDiaImpedidoStamp.
 
 
-%Metodo que vai retornar o ultimo dia impedido
-ultimoDiaImpedido(Doador, UltimoDiaImpedido).
+dataParaString(Stamp,StringData):-
+    stamp_date_time(Stamp, DataString, 'UTC'), 
+    date_time_value(year, DataString, Value3), 
+    date_time_value(month, DataString, Value2), 
+    date_time_value(day, DataString, Value1),
+    string_concat(Value1, "/", Part1), 
+    string_concat(Part1, Value2, Part2),
+    string_concat(Part2, "/", Part3),
+    string_concat(Part3, Value3, StringData).
+
+stringParaData(StringData,Stamp):-
+    atomic_list_concat(DiaMesAno,"/", StringData),
+    nth0(0, DiaMesAno, DiaAtom), atom_number(DiaAtom, Dia),
+    nth0(1, DiaMesAno, MesAtom), atom_number(MesAtom, Mes),
+    nth0(2, DiaMesAno, AnoAtom), atom_number(AnoAtom, Ano),
+    date_time_stamp(date(Ano,Mes,Dia,0,0,0,0,_,_), Stamp).
